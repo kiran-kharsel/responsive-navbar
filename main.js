c813627c3a509a0fa076d87e17298de712419a55
kiran-kharsel <kirankharsel@gmail.com>
2025-12-04 15:35:24 +0530

    fix(css): correct toggle styling behavior

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-icon');
+
+
+menuBtn.addEventListener('click', function(){
+    document.querySelector('.nav-links').classList.toggle('show');
+    console.log('working')
+})
